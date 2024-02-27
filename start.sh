f4a7ccbcbed462f417039c8f86951671132b0251
parc02 <pascalmin49@gmail.com>
2024-02-27 16:32:13 +0900

    1

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup java -jar ~/app/sb-rest/rest-service-0.2.0-SNAPSHOT.jar >> ~/app/sb-rest/log.log &
-echo $! > ~/app/sb-rest/pid.file
+nohup java -jar ~/app/jenkins/rest-service-0.2.0-SNAPSHOT.jar >> ~/app/jenkins/log.log &
+echo $! > ~/app/jenkins/pid.file
